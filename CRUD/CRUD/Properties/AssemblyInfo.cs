f84a40c1611e77be350f925637f2971578c76ad1
XREFS0 <xrefs0@users.noreply.github.com>
2026-09-20 02:27:14 +0200

    Initial release of MASA Customer Management System

---
+++ CRUD/CRUD/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+ 
+ 
+ 
+[assembly: AssemblyTitle("CRUD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MASA")]
+[assembly: AssemblyProduct("MASA Application")]
+[assembly: AssemblyCopyright("Copyright © MASA 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+ 
+ 
+ 
+[assembly: ComVisible(false)]
+
+ 
+[assembly: Guid("1945b2fd-8eed-47fb-81ec-e62b92068beb")]
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
